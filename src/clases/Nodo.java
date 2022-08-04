0aedf9ef9066ee6e5380d360885da994d5920a86
xiLuiis <luistiersen@gmail.com>
2022-08-04 15:18:45 -0500

    Primer commit

---
+++ src/clases/Nodo.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class Nodo {
+    char informacion;
+    Nodo siguiente;
+    
+    
+}
---
+++ src/clases/Pila.java
@@ -0,0 +1,47 @@
+package clases;
+
+public class Pila {
+    private Nodo UltimoValorIngresado;
+    public Pila(){
+        UltimoValorIngresado = null;
+        
+    }
+    
+    public void Insertar(char valor){
+        Nodo nuevo_nodo = new Nodo();
+        nuevo_nodo.informacion = valor;
+        
+        if (UltimoValorIngresado == null) {
+            
+            nuevo_nodo.siguiente = null;
+            UltimoValorIngresado = nuevo_nodo;
+            
+        } else {
+            
+            nuevo_nodo.siguiente = UltimoValorIngresado;
+            UltimoValorIngresado = nuevo_nodo;
+            
+        }
+        
+        
+    }
+    
+    public char Extraer(){
+        
+        if (UltimoValorIngresado != null) {
+            char informacion = UltimoValorIngresado.informacion;
+            UltimoValorIngresado=UltimoValorIngresado.siguiente;
+            return informacion;
+            
+        } else {
+            return Character.MAX_VALUE;
+        }
+        
+    }
+    
+    public boolean PilaVacia(){
+        
+        return UltimoValorIngresado==null;
+        
+    }
+}
